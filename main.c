b84c7c189c5e606a11cf8a283886201a71653c86
silvia44s <167474885+silvia44s@users.noreply.github.com>
2024-11-21 21:28:33 +0100

    Update main.c

--- main.c
+++ main.c
@@ -25,6 +25,10 @@ int main(int argc, char *argv[]) {
     setSourceFile(file);
     NodePtr node = parser();
     //semantic_parse(node);
+    CodeGenerator *cg = cg_init();  // code generator
+    cg_generate_header();
+    generate_block(cg, node);
+    
     freeTree(node);
 
 
