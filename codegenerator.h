c799d7fb00c9819de48cae49ecd8609886104823
silvia44s <167474885+silvia44s@users.noreply.github.com>
2024-12-02 00:43:50 +0100

    Update codegenerator.h

--- codegenerator.h
+++ codegenerator.h
@@ -17,7 +17,6 @@
 #include "parser.h"
 #include "builtinfun.h"
 
-//typedef struct Node* NodePtr;
 
 //struct for code generator
 typedef struct CodeGenerator {
@@ -35,31 +34,30 @@ void cg_generate_header();
 //generates built in functions
 void gen_built_in_fun();
 
-// DEFVAR <var>
-//param@ FRAME - "LF" "GF" "TF"
+// Declares a variable in the specified frame.
 void cg_defvar(CodeGenerator *cg, const char *frame, const char *var_name);
 
-// MOVE <var> <symb>
+// Moves a symbol's value to a variable.
 void cg_move(CodeGenerator *cg, const char *dest_frame, const char *dest_var, const char *src_symb);
 
-// CALL
+// Generates CALL 
 void cg_call(CodeGenerator *cg, const char *label);
 
-// RETURN
+// Generates RETURN
 void cg_return(CodeGenerator *cg);
 
-// frame managment
+// Frame operations: create, push, or pop a frame.
 void cg_createframe(CodeGenerator *cg);
 void cg_pushframe(CodeGenerator *cg);
 void cg_popframe(CodeGenerator *cg);
 
-// label and jump 
+// Generates labels and jumps for control flow.
 void cg_label(CodeGenerator *cg, const char *label);
 void cg_jump(CodeGenerator *cg, const char *label);
 void cg_jumpifeq(CodeGenerator *cg, const char *label, const char *symb1, const char *symb2);
 void cg_jumpifneq(CodeGenerator *cg, const char *label, const char *symb1, const char *symb2);
 
-// arithmetic operations <var> <symb> <symb>
+// Arithmetic operations: ADD, SUB, MUL, DIV, IDIV.
 void cg_add(CodeGenerator *cg, const char *dest, const char *symb1, const char *symb2);
 void cg_sub(CodeGenerator *cg, const char *dest, const char *symb1, const char *symb2);
 void cg_mul(CodeGenerator *cg, const char *dest, const char *symb1, const char *symb2);
@@ -83,7 +81,7 @@ void cg_strlen(CodeGenerator *cg, const char *dest, const char *symb);
 void cg_getchar(CodeGenerator *cg, const char *dest, const char *symb1, const char *symb2);
 void cg_setchar(CodeGenerator *cg, const char *var, const char *symb1, const char *symb2);
 
-// logical operations
+// Logical operations: AND, OR, NOT, LT, GT, EQ.
 void cg_lt(CodeGenerator *cg, const char *var, const char *symb1, const char *symb2);
 void cg_gt(CodeGenerator *cg, const char *var, const char *symb1, const char *symb2);
 void cg_eq(CodeGenerator *cg, const char *var, const char *symb1, const char *symb2);
@@ -98,12 +96,12 @@ void generate_function(CodeGenerator *cg, NodePtr fun_node);
 void generate_function_call(CodeGenerator *cg, NodePtr fn_node);
 
 //generate built in function call code
-void generate_builtin_call(CodeGenerator *cg, NodePtr ifj_callNode);
+void generate_builtin_call(CodeGenerator *cg, NodePtr ifj_callNode, NodePtr storeNode);
 
-//params
+//generate params
 void cg_function_def_params(CodeGenerator *cg, NodePtr param_list);
 
-// generates code block  
+// Generates a block of code (e.g., within a loop or function). 
 void generate_block(CodeGenerator *cg, NodePtr block_node);
 
 // generates return statement
@@ -121,27 +119,46 @@ void generate_while(CodeGenerator *cg, NodePtr while_node);
 // generates variable or constant declaration
 void generate_declaration(CodeGenerator *cg, NodePtr dcl_node);
 
-// generates expression
+// Evaluates and generates code for expressions.
 void generate_expression(CodeGenerator *cg, NodePtr expr_node, char *result);
 
-// helper function for generating temporary variable names
+// Generates a temporary variable for storing intermediate results.
 char* generate_temp_var(CodeGenerator *cg, NodePtr node);
 
 // generates the string literal 
 char* cg_literal(NodePtr node);
 
-//easy way to write any instruction
+// Writes a formatted instruction to the output stream.
 void cg_write_instruction(CodeGenerator *cg, const char *format, ...);
 
-//function for rewriting string
+// Rewrites a string for escaping and formatting.
 char* rewrite_string(const char *input);
 
-
-
+// Duplicates a string with memory allocation.
 char* my_strdup(const char *str);
+
+// Checks if a variable is already declared.
 bool is_variable_declared(const char *name);
+
+// Adds a variable to the symbol table.
 void add_variable_to_symbol_table(const char *name);
+
+// Reallocates memory safely.
 void* safe_realloc(void *ptr, size_t new_size);
+
+// Formats a string dynamically and returns it.
 char *format_string(const char *format, ...);
+
+// Resets the symbol table for managing declared variables.
+void reset_symbol_table();
+
+// Adds a variable and its type to the type table.
+void add_variable_to_type_table(const char *name, DataType type);
+
+// Retrieves the type of a variable.
+DataType get_variable_type(const char *name);
+
+// Prints declared variables for debugging.
+void  printDeclaredVariables();
 
 #endif // CODEGENERATOR_H
